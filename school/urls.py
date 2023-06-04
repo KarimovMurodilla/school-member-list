2da119dc322994baf7378b2ac7c16c045210ad6e
KarimovMurodilla <karimovmurodilla15@gmail.com>
2023-06-04 19:38:47 +0500

    some changes

--- school/urls.py
+++ school/urls.py
@@ -21,8 +21,8 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('members.urls')),
+    path('admin/', admin.site.urls),
 ]
 
 if settings.DEBUG:
