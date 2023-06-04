dac992037a7dde64146f7dc5669f138903188f1a
KarimovMurodilla <karimovmurodilla15@gmail.com>
2023-06-04 18:00:30 +0500

    used postgres

--- school/settings.py
+++ school/settings.py
@@ -77,10 +77,20 @@ WSGI_APPLICATION = 'school.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'XWrWjEgS83eAJh3ALTD7',
+        'HOST': 'containers-us-west-68.railway.app',
+        'PORT': '6649',
     }
 }
 
